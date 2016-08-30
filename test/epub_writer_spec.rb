785d4735a08ad07c14c3c81f387df390f2cf0dbc
Tom Gilligan <tomgilligan@users.noreply.github.com>
2016-08-30 17:34:59 +1000

    Assert that IO has been written to

--- test/epub_writer_spec.rb
+++ test/epub_writer_spec.rb
@@ -1,14 +1,44 @@
 require 'test_helper'
 
 describe EPUBWriter do
-  around do |test|
-    Tempfile.create(['test', EPUBWriter::FILENAME_EXTENSION]) do |temp_file|
-      @temp_file = temp_file
-      test.call
+  before do
+    @epub_writer = EPUBWriter.new
+  end
+
+  describe 'writes a file' do
+    around do |test|
+      Tempfile.create(['test', EPUBWriter::FILENAME_EXTENSION]) do |temp_file|
+        @temp_file = temp_file
+        test.call
+      end
+    end
+
+    before do
+      @epub_writer.write(@temp_file)
+      @temp_file.rewind
+      @file_contents = @temp_file.read
+    end
+
+    it 'file exists' do
+      assert File.exist?(@temp_file)
+    end
+
+    it 'file contents' do
+      refute_empty @file_contents
     end
   end
 
-  it 'writes a file' do
-    EPUBWriter.new.write(@temp_file)
+  describe 'writes to string io' do
+    before do
+      @string_io = StringIO.new
+
+      @epub_writer.write(@string_io)
+      @string_io.rewind
+      @file_contents = @string_io.read
+    end
+
+    it 'file contents' do
+      refute_empty @file_contents
+    end
   end
 end
